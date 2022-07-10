751bfc9bb7a2032e71e0a2f8ae21f04b0935baf3
Utilisateur <ayoub.mouak.2015@gmail.com>
2022-07-10 13:56:06 +0100

    automate the creation and the use of tokens by terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,6 +2,8 @@ provider "aws" {
 
   region  = var.region-monitoring
   profile = var.profile
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   alias   = "region-monitoring"
 }
 
