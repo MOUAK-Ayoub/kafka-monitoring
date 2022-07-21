f75983675082fe3700935cf3df9e9efa91b90a42
Utilisateur <ayoub.mouak.2015@gmail.com>
2022-07-22 01:26:53 +0200

    REmove profil in provider.tf ( not accepted by jenkins)

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 
   region  = var.region-monitoring
-  profile = var.profile
+  //profile = var.profile
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   alias   = "region-monitoring"
